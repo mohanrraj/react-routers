14caea440f0498b7297138594034dde77676b684
mohanrraj <mohanrraj1994@gmail.com>
2023-08-22 19:02:55 +0530

    React routes concept added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {Routes, Route, Link} from "react-router-dom"
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import Footer from "./components/Footer";
+import Post from "./components/Post";
+import PostItem from "./components/PostItem";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/postpage">PostPage</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/newpost" element={<NewPost />}></Route>
+        <Route path="/postpage" element={<PostItem />}>
+          <Route index element={<PostPage />}></Route>
+          <Route path=":id" element={<Post />}></Route>
+          <Route path="newpost" element={<NewPost />}></Route>
+        </Route>
+      </Routes>
+      {/* <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/components/PostPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const PostPage = () => {
+  return (
+    <main>
+        <h1>PostPage</h1>
+        
+    </main>
+  )
+}
+
+export default PostPage
